498909d21be02e1c5983020724ba7a6e069ffece
buketyagmuraltin <buketyagmuraltin@hotmail.com>
2021-02-19 15:30:07 +0300

    +created: first lesson script

---
+++ Assets/Scripts/FirstLessonScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LessonBasic
+{
+public class FirstLessonScript : MonoBehaviour
+{
+
+    /// <summary>
+    /// C# variables
+    /// </summary>
+    // Start is called before the first frame update
+    public int tamSayi = 1;
+    [SerializeField] int tamSayi2 = 100;
+    float ondalikSayi = 0.25f;
+    float ondalikSayi2 = 4.2125f;
+    bool dogryYanlis = true;
+    bool dogruYanlis2 = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
+
