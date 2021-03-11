9d0d3cd9adc51bcfe15c512d3960aea1d1ca357a
buketyagmuraltin <buketyagmuraltin@hotmail.com>
2021-03-11 10:19:19 +0300

    +added: basic movement for camera

---
+++ Assets/TopDownsShooter/Scripts/Camera/CameraSettings.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDpwnShooter
+{
+    [CreateAssetMenu(menuName = "Topdow Shooter/Camera/Camera Settings")]
+    public class CameraSettings : ScriptableObject
+    {
+        [Header("Position")]
+        [SerializeField] private float _rotationLerpSpeed = 3;
+        public float RotationLerpSpeed { get { return _rotationLerpSpeed;} }
+
+        [Header("Position")]
+        [SerializeField] private Vector3 _positionOffSet;
+        public Vector3 PositionOffSet { get { return _positionOffSet; } }
+
+        [SerializeField] private float _positionLerp = 1;
+        public float PositionLerp { get { return _positionLerp; } }
+
+
+
+
+    }
+}
